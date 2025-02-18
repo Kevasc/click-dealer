670bb330fa2e4b336e0012e1f2468526cfcc3da5
kevas cole <colekevas@gmail.com>
2025-02-18 11:12:22 +0000

    Created a toggle button to hide or display the middle column on click

--- homepage.js
+++ homepage.js
@@ -3,3 +3,13 @@ const middleColumn = () => {
 };
 
 middleColumn();
+
+const hideColumn = (columnId) => {
+  console.log("id =", columnId);
+  let column = document.getElementById(columnId);
+  if (column.style.display === "none") {
+    column.style.display = "block";
+  } else {
+    column.style.display = "none";
+  }
+};
