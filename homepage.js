8c45bf89f2737db44c335350a44f8e29235e2f59
kevas cole <colekevas@gmail.com>
2025-02-18 18:29:11 +0000

    Display "block" was messing up the middle column when toggled back on, changed to flex - problem solved

--- homepage.js
+++ homepage.js
@@ -8,7 +8,7 @@ const hideColumn = (columnId) => {
   console.log("id =", columnId);
   let column = document.getElementById(columnId);
   if (column.style.display === "none") {
-    column.style.display = "block";
+    column.style.display = "flex";
   } else {
     column.style.display = "none";
   }
